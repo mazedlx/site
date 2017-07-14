204c52081c512f71eb334a3919301cc2dad15685
Christian Leo-Pernold <mazedlx@gmail.com>
2017-07-14 20:51:00 +0200

    Added legal stuff.

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -6,13 +6,27 @@
         <div class="col-lg-offset-4 col-lg-4">
             <p>
                 <i class="fa fa-user fa-fw" aria-hidden="true"></i> DI(FH) Christian Leo-Pernold <br>
-                <i class="fa fa-home fa-fw" aria-hidden="true"></i> Glockenblumengasse 36/6/2, 1220 Wien <br/>
-                <i class="fa fa-mobile fa-fw" aria-hidden="true"></i> +43 660 456 3193 <br/>
-                <i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i><a href="mailto:mazedlx@gmail.com"> mazedlx@gmail.com</a> <br/>
-                <i class="fa fa-github fa-fw" aria-hidden="true"></i><a href="https://github.com/mazedlx"> github.com/mazedlx </a> <br/>
-                <i class="fa fa-twitter fa-fw" aria-hidden="true"></i><a href="https://twitter.com/mazedlx"> @mazedlx </a> <br/>
-                <i class="fa fa-facebook-official fa-fw" aria-hidden="true"></i><a href="https://facebook.com/mazedlx"> mdxwebprod </a> <br/>
+                <i class="fa fa-home fa-fw" aria-hidden="true"></i> Glockenblumengasse 36/6/2, 1220 Wien<br>
+                <i class="fa fa-mobile fa-fw" aria-hidden="true"></i> +43 660 456 3193<br>
+                <i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i><a href="mailto:mazedlx@gmail.com"> mazedlx@gmail.com</a><br>
+                <i class="fa fa-github fa-fw" aria-hidden="true"></i><a href="https://github.com/mazedlx"> github.com/mazedlx </a><br>
+                <i class="fa fa-twitter fa-fw" aria-hidden="true"></i><a href="https://twitter.com/mazedlx"> @mazedlx </a><br>
+                <i class="fa fa-facebook-official fa-fw" aria-hidden="true"></i><a href="https://facebook.com/mazedlx"> mdxwebprod </a><br>
             </p>
         </div><!-- col -->
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-lg-offset-3 col-lg-6">
+            <p>
+                <a href="https://www.wkoecg.at/Web/Ecg.aspx?FirmaID=f509fee5-7a6f-4f40-9212-67694b8dab33">
+                    Informationspflicht lt. &sect;5 E-Commerce Gesetz,<br>
+                    &sect;14 Unternehmensgesetzbuch bzw. &sect;63 Gewerbeordnung, und<br>
+                    Offenlegungspflicht lt. &sect;25 Mediengesetz.
+                </a>
+            </p>
+        </div>
     </div>
 </div><!-- container -->
