a9a028904df9a5daa636cfb0540bb6875944072e
Christian Leo-Pernold <mazedlx@gmail.com>
2018-03-22 10:20:44 +0100

    Added envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,11 @@
 @servers(['web' => ['maze@mazedlx.net']])
 
 @task('deploy', ['on' => 'web'])
-    cd /var/www/html/krankenhausbewertung
+    cd /var/www/html/site
     php artisan down
     git stash
     git pull
+    php artisan migrate:fresh --seed
     yarn prod
     php artisan up
     php artisan queue:work &!
