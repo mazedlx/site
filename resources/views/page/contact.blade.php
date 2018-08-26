677fb684807c9eb818feeced854d0ca3fd16b3c3
Christian Leo-Pernold <mazedlx@gmail.com>
2018-08-26 22:37:30 +0200

    wip

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -1,7 +1,7 @@
 <div id="contact" class="pb-8">
     <div class="text-4xl py-4 text-right font-serif">Kontakt</div>
     <div class="flex flex-col">
-         <p class="text-xl leading-loose  font-serif">
+         <p class="text-xl leading-loose font-serif">
             Sie wollen Ihr nächstes Projekt mit uns starten? Großartig! Füllen Sie das Formular aus oder kontaktieren Sie uns unter <a href="tel:+436604563193" class="text-blue-light">+43 660 4563193</a> oder <a href="mailto:mazedlx.net?subject=Mein+neues+Projekt" class="text-blue-light">mazedlx@gmail.com</a>
          </p>
 
