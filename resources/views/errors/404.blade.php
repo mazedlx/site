0042c8faab593a27563749e67160845b78878ea3
Christian Leo-Pernold <mazedlx@gmail.com>
2018-08-27 08:22:41 +0200

    Fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="de">
+@extend('layouts.page')
 
-    @include('page.head')
-
-    <body style="background: url('images/404.jpg')">
-        <div class="container" style="margin-top: 70px;">
-            <div class="jumbotron">
-                <h1>404 - Die Seite existiert leider nicht. </h1>
-                <a href="https://mazedlx.net" class="btn btn-lg btn-primary">Zur&uuml;ck zur Seite</a>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+404 - Seite nicht gefunden
+@endsection
