ecf6ce42a45fefa8dabcccf1354a6d434c0d07fa
Christian Leo-Pernold <mazedlx@gmail.com>
2020-05-01 19:09:36 +0200

    Fix jitter when hovering.

--- resources/views/page/nav.blade.php
+++ resources/views/page/nav.blade.php
@@ -4,19 +4,19 @@
         <button id="nav_btn" type="button">@svg('menu', 'h-6 w-6 fill-current')</button>
     </div>
     <div class="hidden flex md:flex flex-col md:flex-row md:justify-around md:w-2/3" id="nav_links">
-        <div class="flex-auto py-2 px-1 text-center nav-item">
+        <div class="flex-auto py-2 px-1 text-center nav-item border border-t-0 border-l-0 border-r-0 border-b-2 border-transparent">
             <a class="no-underline text-gray-700 text-lg font-serif" title="__('Leistungen')" href="#services">{{ __('Leistungen') }}</a>
         </div>
-        <div class="flex-auto py-2 px-1 text-center nav-item">
+        <div class="flex-auto py-2 px-1 text-center nav-item border border-t-0 border-l-0 border-r-0 border-b-2 border-transparent">
             <a class="no-underline text-gray-700 text-lg font-serif" title="__('Referenzen')" href="#portfolio">{{ __('Referenzen') }}</a>
         </div>
-        <div class="flex-auto py-2 px-1 text-center nav-item">
+        <div class="flex-auto py-2 px-1 text-center nav-item border border-t-0 border-l-0 border-r-0 border-b-2 border-transparent">
             <a class="no-underline text-gray-700 text-lg font-serif" title="__('Kontakt')" href="#contact">{{ __('Kontakt') }}</a>
         </div>
-        <div class="flex-auto py-2 px-1 text-center nav-item">
+        <div class="flex-auto py-2 px-1 text-center nav-item border border-t-0 border-l-0 border-r-0 border-b-2 border-transparent">
             <a class="no-underline text-gray-700 text-lg font-serif" title="__('Impressum')" href="#imprint">{{ __('Impressum') }}</a>
         </div>
-        <div class="flex-auto py-2 px-1 text-center nav-item">
+        <div class="flex-auto py-2 px-1 text-center nav-item border border-t-0 border-l-0 border-r-0 border-b-2 border-transparent">
             <a class="no-underline text-gray-700 text-lg font-serif" title="__('Blog')" href="https://blog.mazedlx.net/" target="_blank">{{ __('Blog') }}</a>
         </div>
         <div class="flex-auto py-2 px-1 text-center nav-item">
