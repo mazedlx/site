0108c490e1303704276b33b4e2563d438146c6a2
Christian Leo-Pernold <mazedlx@gmail.com>
2020-05-09 21:34:44 +0200

    Split envoy into tasks.

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,24 +1,50 @@
 @servers(['web' => ['maze@mazedlx.net']])
 
-@task('deploy', ['on' => 'web'])
+@story('deploy', ['on' => 'web'])
+    cd
+    down
+    git
+    composer
+    migrate
+    npm
+    up
+@endstory
+
+@story('quick', ['on' => 'web'])
+    cd
+    down
+    git
+    up
+@endstory
+
+@task('cd')
     cd /var/www/html/site
-    php artisan down
-    git stash
-    git pull
-    composer install --no-dev
-    composer update --no-dev
-    php artisan migrate:fresh --seed --force
-    npm install
-    npm run prod
-    php artisan optimize
-    php artisan up
 @endtask
 
-@task('quick', ['on' => 'web'])
-    cd /var/www/html/site
+@task('down')
     php artisan down
+@endtask
+
+@task('git')
     git stash
     git pull
+@endtask
+
+@task('composer')
+    composer install --no-dev
+    composer update --no-dev
+@endtask
+
+@task('migrate')
+    php artisan migrate:fresh --seed --force
+@endtask
+
+@task('npm')
+    npm install
+    npm run prod
+@endtask
+
+@task('up')
     php artisan optimize
     php artisan up
 @endtask
