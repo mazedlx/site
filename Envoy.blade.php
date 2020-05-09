02e3b40540452491c27dfd188407be1f6e25d1e1
Christian Leo-Pernold <mazedlx@gmail.com>
2020-05-09 21:55:01 +0200

    Revert Envoy. Mv ignition to deps.

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,50 +1,24 @@
 @servers(['web' => ['maze@mazedlx.net']])
 
-@story('deploy')
-    htdocs
-    down
-    git
-    composer
-    migrate
-    npm
-    up
-@endstory
-
-@story('quick')
-    htdocs
-    down
-    git
-    up
-@endstory
-
-@task('htdocs')
+@task('deploy', ['on' => 'web'])
     cd /var/www/html/site
-@endtask
-
-@task('down')
     php artisan down
-@endtask
-
-@task('git')
     git stash
     git pull
-@endtask
-
-@task('composer')
     composer install --no-dev
     composer update --no-dev
-@endtask
-
-@task('migrate')
     php artisan migrate:fresh --seed --force
-@endtask
-
-@task('npm')
     npm install
     npm run prod
+    php artisan optimize
+    php artisan up
 @endtask
 
-@task('up')
+@task('quick', ['on' => 'web'])
+    cd /var/www/html/site
+    php artisan down
+    git stash
+    git pull
     php artisan optimize
     php artisan up
 @endtask
