b04cc31e51d2cf51fd0ace95b998dbd3905fbb9e
Christian Leo-Pernold <mazedlx@gmail.com>
2021-01-31 14:19:51 +0100

    Upated uses.

--- resources/views/uses.blade.php
+++ resources/views/uses.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.page')
 
 @section('content')
-<div class="container mx-auto w-2/3 mt-8">
-    <h1 class="text-6xl font-mono">mazedlx uses &hellip;</h1>
+<div class="container w-2/3 mx-auto mt-8">
+    <h1 class="font-mono text-6xl">mazedlx uses &hellip;</h1>
     A - somewhat - up to date list of things I work with.
 
-    <h2 class="text-4xl font-thin mt-8">Hardware</h2>
+    <h2 class="mt-8 text-4xl font-thin">Hardware</h2>
     <ul>
-        <li>13" MacBook Pro (Late 2012, 8GB RAM, 500GB SSD)</li>
+        <li>15" MacBook Pro (2017, 16GB RAM, 500GB SSD)</li>
         <li>iPhone Xs</li>
         <li>Beats Studio 3 Wireless</li>
-        <li>Beats X</li>
-        <li>PowerBeats 3</li>
+        <li>Apple AirPods 2nd Generation</li>
         <li>Apple Watch Series 2</li>
+        <li>Røde Podcaster Mic/PSM1 Shock Mount/PSA1 Boom Arm</li>
+        <li>Two Acer B247Y (landscape/portrait mode, Full HD)</li>
+        <li>CalDigit Thunderbolt 3 Mini Dock</li>
     </ul>
-    
-    <h2 class="text-4xl font-thin mt-8">Software</h2>
+
+    <h2 class="mt-8 text-4xl font-thin">Software</h2>
     <ul>
         <li>Visual Studio Code with a bunch of plugins:
             <ul>
                 <li>PHP Intelephense</li>
                 <li>Project Manager</li>
                 <li>Better PHPUnit</li>
-                <li>Cobalt2 Theme</li>
+                <li>Better Keybindings</li>
+                <li>Palenight Theme</li>
                 <li>ESLint</li>
                 <li>Import Cost</li>
                 <li>File Utils</li>
@@ -31,11 +34,12 @@
                 <li>and more &hellip;</li>
             </ul>
         </li>
-        <li>Sublime Text 3</li>
-        <li>Hyper</li>
-        <li>SequelPro</li>
-        <li>Bear</li>
-        <li>Deckset</li>
+        <li><a href="https://hyper.is/">Hyper</a></li>
+        <li><a href="https://www.sublimetext.com/">Sublime Text 3</a></li>
+        <li><a href="https://www.sublimemerge.com/">Sublime Merge</a></li>
+        <li><a href="https://www.tableplus.io/">Table Plus</a></li>
+        <li><a href="https://bear.app/">Bear</a></li>
+        <li><a href="https://www.deckset.com/">Deckset</a></li>
     </ul>
 </div>
 @endsection
